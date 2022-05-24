35277d34f3e78b4bc0776385f1bf207981164763
Matteo Orlando <matteo.orlando.1993@gmail.com>
2022-05-24 15:22:37 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		vue({
+		template: {
+		  compilerOptions: {
+			// treat any tag that starts with ion- as custom elements
+			isCustomElement: tag => tag.startsWith('ion-')
+		  }
+		}
+	  })],
+});
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+// Import AgnosticUI global common & component CSS
+import "agnostic-vue/dist/common.min.css";
+import "agnostic-vue/dist/index.css";
+import { Header, HeaderNav, HeaderNavItem } from "agnostic-vue";
+</script>
+<template>
+  <h3 class="mbe12">Header defaults to space between</h3>
+  <Header>
+    <template v-slot:logoleft>
+      <a href="https://web.dev/">web.dev</a>
+    </template>
+    <template v-slot:headernav>
+      <HeaderNav>
+        <HeaderNavItem>
+          <a href="https://css-tricks.com/">CSS-Tricks</a>
+        </HeaderNavItem>
+        <HeaderNavItem>
+          <a href="https://developer.mozilla.org/en-US/">MDN</a>
+        </HeaderNavItem>
+      </HeaderNav>
+    </template>
+    <template v-slot:logoright>
+      <a href="https://www.freecodecamp.org/">freeCodeCamp</a>
+    </template>
+  </Header>
+</template>
